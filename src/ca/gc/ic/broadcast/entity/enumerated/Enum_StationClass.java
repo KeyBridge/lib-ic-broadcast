cfd43ef429e414c072128403854fccbc746c2cb7
Jesse Caulfield <jesse@caulfield.org>
2012-09-13 08:27:41 -0400

    improve documentation in enum_stationClass

--- src/ca/gc/ic/broadcast/entity/enumerated/Enum_StationClass.java
+++ src/ca/gc/ic/broadcast/entity/enumerated/Enum_StationClass.java
@@ -16,37 +16,48 @@
 package ca.gc.ic.broadcast.entity.enumerated;
 
 /**
- *
+ * An enumeration of the allowed 'CLASS' field values in the Canada station
+ * tables 'am_statio', 'fm_statio', 'tv_statio' and 'sdars'.
+ * <p/>
+ * To prevent Java language conflict the table field is named 'clazz' and the
+ * Java object (this) is named 'Enum_StationClass'.
+ * <p/>
+ * Some classes are used across service types. For example. 'A' is used for AM,
+ * FM and TV stations.
+ * <p/>
  * @author jesse
  */
 public enum Enum_StationClass {
 
   /**
-   * Class A
+   * Class A. Present in AM stations. Present in FM and TV stations. For TV
+   * stations this is an NTSC UHF station.
    */
   A("Class A"),
   /**
-   * Class A1
+   * Class A1. Present in FM stations.
    */
   A1("Class A1"),
   /**
-   * Class B
+   * Class B. Present in AM, FM and TV stations. For TV stations this is an NTSC
+   * UHF station.
    */
   B("Class B"),
   /**
-   * Class B1
+   * Class B1. Present in FM stations.
    */
   B1("Class B1"),
   /**
-   * Class C
+   * Class C. Present in AM, FM and TV stations. For TV stations this is an NTSC
+   * UHF station.
    */
   C("Class C"),
   /**
-   * Class C1
+   * Class C1. Present in FM stations.
    */
   C1("Class C1"),
   /**
-   * Class C2
+   * Class C2. Present in FM stations.
    */
   C2("Class C2"),
   /**
@@ -54,43 +65,47 @@ public enum Enum_StationClass {
    */
   CC("Carrier Current"),
   /**
-   * American Ten Watter
+   * American Low Power Station. Present in FM and TV stations. For TV stations
+   * this is an NTSC Class D low power TV station.
    */
-  D("Low Power TV"),
+  D("Low Power"),
   /**
-   * Micro-Wave Fixed
+   * Micro-Wave Fixed. Present in TV stations.
    */
   F("Micro-Wave Fixed"),
   /**
-   * Low Power
+   * Low Power. Present in AM, FM and TV stations. For TV stations this is an
+   * Low power NTSC VHF/UHF station.
    */
   LP("Low Power"),
   /**
-   * UHF Regular (U.S. Analog)
+   * UHF Regular (U.S. Analog). Present in TV stations.
    */
   N("UHF Regular (U.S. Analog)"),
   /**
-   * Regular VHF & MCTV
+   * Regular VHF & MCTV. Present in TV stations.
    */
   R("Regular VHF & MCTV"),
   /**
-   * High-Power TV
+   * High-Power TV Analog. Present in TV stations.
    */
-  S("High-Power TV"),
+  S("High-Power TV Analog"),
   /**
-   * S-Dars
+   * Satelite Digital Audio Radio (S-Dars)
    */
-  SA("Sattelite Digital Audio Radio"),
+  SA("Satelite Digital Audio Radio"),
   /**
-   * Very Low Power
+   * Very Low Power. Present in TV stations. For TV stations this is a digital
+   * transition station (low VHF band).
    */
-  VL("Very Low Power"),
+  VL("Very Low Power Digital"),
   /**
-   * Very Low Power TV Analog
+   * Very Low Power TV Analog. Present in FM and TV stations.
    */
-  VLP("Very Low Power TV Analog"),
+  VLP("Very Low Power Analog"),
   /**
-   * DTV Upper VHF
+   * DTV Upper VHF. For TV stations this is a digital transition station (upper
+   * VHF band)
    */
   VU("DTV Upper VHF");
   private String description;
@@ -104,10 +119,10 @@ public enum Enum_StationClass {
   }
 
   /**
-   * Find an enumBanner object by its 2-character name code.
+   * Find an Enum_StationClass object by its 2 or 3-character database code.
    * <p/>
-   * @param dbCode the 2-character code
-   * @return
+   * @param dbCode The 2 or 3-character database code (not case-sensistive)
+   * @return null if no match
    */
   public static Enum_StationClass findByDbCode(String dbCode) {
     for (Enum_StationClass enum_Banner : Enum_StationClass.values()) {
