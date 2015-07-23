e6a5df9efe6a4948d5758beb05f9b2aa707acdcc
Jesse Caulfield <jesse@caulfield.org>
2015-07-23 10:23:55 -0400

    simple toString

--- src/main/java/ca/gc/ic/lib/bdbs/entity/enumerated/ECanadaStationType.java
+++ src/main/java/ca/gc/ic/lib/bdbs/entity/enumerated/ECanadaStationType.java
@@ -1,5 +1,5 @@
-/* 
- * Copyright (C) 2014 Key Bridge Global LLC 
+/*
+ * Copyright (C) 2014 Key Bridge Global LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -63,6 +63,6 @@ public enum ECanadaStationType {
 
   @Override
   public String toString() {
-    return name() + " - " + description;
+    return name();
   }
 }
