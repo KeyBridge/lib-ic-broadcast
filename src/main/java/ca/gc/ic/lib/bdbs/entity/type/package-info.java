562e7d04bc6762c715f8cd0a9d3630ed91f0b916
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2018-09-30 15:50:22 -0400

    add package-info to declare XML type

---
+++ src/main/java/ca/gc/ic/lib/bdbs/entity/type/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2018 Key Bridge. All rights reserved. Use is subject to license
+ * terms.
+ *
+ * This software code is protected by Copyrights and remains the property of
+ * Key Bridge and its suppliers, if any. Key Bridge reserves all rights in and to
+ * Copyrights and no license is granted under Copyrights in this Software
+ * License Agreement.
+ *
+ * Key Bridge generally licenses Copyrights for commercialization pursuant to
+ * the terms of either a Standard Software Source Code License Agreement or a
+ * Standard Product License Agreement. A copy of either Agreement can be
+ * obtained upon request by sending an email to info@keybridgewireless.com.
+ *
+ * All information contained herein is the property of Key Bridge and its
+ * suppliers, if any. The intellectual and technical concepts contained herein
+ * are proprietary.
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ic.gc.ca/bdbs/entity/type", elementFormDefault = XmlNsForm.QUALIFIED)
+package ca.gc.ic.lib.bdbs.entity.type;
+
+import javax.xml.bind.annotation.XmlNsForm;
